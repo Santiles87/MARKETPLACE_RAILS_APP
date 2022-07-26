33b5ec24e3d01c5ac413cc53edba990d050e6ed8
fernandaquetcia <fernandaquetcia@gmail.com>
2022-07-26 16:55:21 -0600

    created schema and offers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_07_26_225233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.string "product_name"
+    t.float "price"
+    t.string "description"
+    t.text "image"
+    t.text "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
