e644e7f23008dfab275fa589d0a36200c6b40260
fernandaquetcia <fernandaquetcia@gmail.com>
2022-07-26 17:06:11 -0600

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  resources :offers, only: %i[index new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
