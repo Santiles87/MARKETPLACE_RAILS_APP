61aa026721a390abd4e8076279568937848bce25
fernanda braga <fernanda.gabriela.braga@gmail.com>
2022-08-02 20:53:21 -0400

    avatar photo added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   ## ...Avatar Attachment...##
-  has_one_attached :avatar
+  has_one_attached :photo
 
-  validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
+  validates :photo, file_size: { less_than_or_equal_to: 5.megabytes },
             file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
 end
