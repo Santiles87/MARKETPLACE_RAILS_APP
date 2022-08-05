431623a0bb6ae7c36a75f0faa21769f902f84ffd
fernanda braga <fernanda.gabriela.braga@gmail.com>
2022-08-04 19:57:40 -0600

    profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,4 @@ class UsersController < ApplicationController
   def avatar_params
     params.require(:user).permit(:title, :body, :avatar)
   end
-  
-  def show
-    @user = User.find(params[:id])
-    @user_pages = @user.pages
-  end
-
 end
